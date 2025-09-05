b8e0810f8188702f205a91e562dd1cb2c123cff5
Ihor Solodrai <isolodrai@meta.com>
2025-09-05 16:27:41 -0700

    Fix base branch key in guess_pr()
    Fix a dormant bug in guess_pr(): wrong base branch key has been used
    for lookup of synced PRs.
    
    Apparently this code path almost never executed until e28008d, which
    removed another level of "cache" with lookup by name.
    
    Differential Revision: D79693152
    
    Reviewed-by: Jordan Rome <jordalgo@meta.com>
    Signed-off-by: Ihor Solodrai <isolodrai@meta.com>
    (cherry picked from commit 6205066bbe8a25337fe7b60c8dc4dad275d67c54)
    [ Backport note: e28008d above is 8010f9c on main. This branch does not
      have it, and this fix does not need it. guess_pr() first looks for a
      PR with the series title. If there is none, it looks the PR up by
      its branches. That lookup used repo_branch (e.g. nvme-7.3) as the
      base instead of repo_pr_base_branch (nvme-7.3_base), so it never
      found anything. The title lookup misses renamed series, and PRs
      opened by other GitHub accounts because it only searches this
      account's PRs. Upstream wrote 8010f9c because 88cb274 changed how
      branches are named, so picking 88cb274 here would need 8010f9c too. ]

--- tests/test_branch_worker.py
+++ tests/test_branch_worker.py
@@ -641,7 +641,7 @@ class TestBranchWorker(unittest.IsolatedAsyncioTestCase):
         series = Series(self._pw, SERIES_DATA)
         sentinel = random.random()
         self._bw.all_prs[mybranch] = {}
-        self._bw.all_prs[mybranch][TEST_REPO_BRANCH] = [sentinel]
+        self._bw.all_prs[mybranch][TEST_REPO_PR_BASE_BRANCH] = [sentinel]
         pr = await self._bw._guess_pr(series, mybranch)
         self.assertEqual(sentinel, pr)
 
@@ -658,7 +658,7 @@ class TestBranchWorker(unittest.IsolatedAsyncioTestCase):
 
         sentinel = random.random()
         self._bw.all_prs[mybranch] = {}
-        self._bw.all_prs[mybranch][TEST_REPO_BRANCH] = [sentinel]
+        self._bw.all_prs[mybranch][TEST_REPO_PR_BASE_BRANCH] = [sentinel]
         pr = await self._bw._guess_pr(series, mybranch)
         self.assertEqual(sentinel, pr)
 
